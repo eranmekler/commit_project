68e4267ac12e875836e2adfba8ab74e699bfbb43
eran <meklereran@gmail.com>
2023-07-20 21:47:30 +0300

    uncomentet and user_pool varification

--- dynamo_db.tf
+++ dynamo_db.tf
@@ -1,22 +1,22 @@
-#resource "aws_dynamodb_table" "commit_dynamo_db_table" {
-#  name           = "commit_db"
-#  hash_key       = "Username"  # partition key
-#  range_key      = "Timestamp" # sort key
-#  write_capacity = 20
-#  read_capacity  = 20
-#
-#   attribute {
-#    name = "Username"
-#    type = "S"
-#  }
-#
-#  attribute {
-#    name = "Timestamp"
-#    type = "S"
-#  }
-#
-#  tags = {
-#    Name        = "commit"
-#    Environment = "production"
-#  }
-#}
+resource "aws_dynamodb_table" "commit_dynamo_db_table" {
+  name           = "commit_db"
+  hash_key       = "Username"  # partition key
+  range_key      = "Timestamp" # sort key
+  write_capacity = 20
+  read_capacity  = 20
+
+   attribute {
+    name = "Username"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "commit"
+    Environment = "production"
+  }
+}
--- ssm_parameter.tf
+++ ssm_parameter.tf
@@ -1,23 +1,23 @@
-#resource "aws_ssm_parameter" "Greeting_1" {
-#  name  = "Greeting_1"
-#  type  = "String"
-#  value = "Hello"
-#}
-#
-#resource "aws_ssm_parameter" "Greeting_2" {
-#  name  = "Greeting_2"
-#  type  = "String"
-#  value = "Shalom"
-#}
-#
-#resource "aws_ssm_parameter" "Greeting_3" {
-#  name  = "Greeting_3"
-#  type  = "String"
-#  value = "Hi"
-#}
-#
-#resource "aws_ssm_parameter" "Greeting_4" {
-#  name  = "Greeting_4"
-#  type  = "String"
-#  value = "Marhaba"
-#}
+resource "aws_ssm_parameter" "Greeting_1" {
+  name  = "Greeting_1"
+  type  = "String"
+  value = "Hello"
+}
+
+resource "aws_ssm_parameter" "Greeting_2" {
+  name  = "Greeting_2"
+  type  = "String"
+  value = "Shalom"
+}
+
+resource "aws_ssm_parameter" "Greeting_3" {
+  name  = "Greeting_3"
+  type  = "String"
+  value = "Hi"
+}
+
+resource "aws_ssm_parameter" "Greeting_4" {
+  name  = "Greeting_4"
+  type  = "String"
+  value = "Marhaba"
+}
