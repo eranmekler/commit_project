1f6b8671fdb469aaca055ef30e6b48bb1b533e2a
eran <meklereran@gmail.com>
2023-07-20 21:50:59 +0300

    uncomentet and user_pool varification

--- user_pool.tf
+++ user_pool.tf
@@ -2,7 +2,7 @@ resource "aws_cognito_user_pool" "commit_users" {
   name = "commit_users"
 
   #configure sign-in options
-  username_attributes = ["username"]
+  username_attributes = ["email"]
   auto_verified_attributes = ["email"]
 
   # allow non-admin user to create new users
@@ -10,11 +10,6 @@ resource "aws_cognito_user_pool" "commit_users" {
     allow_admin_create_user_only = false
   }
 
-  schema {
-    attribute_data_type = ""
-    name                = ""
-
-  }
 
   username_configuration {
     case_sensitive = false
@@ -23,29 +18,24 @@ resource "aws_cognito_user_pool" "commit_users" {
   # configuring the password policy
   password_policy {
     minimum_length = 8
-    require_lowercase = anytrue()
-    require_uppercase = anytrue()
-    require_numbers = anytrue()
-    require_symbols = anytrue()
+    require_lowercase = anytrue([true])
+    require_uppercase = anytrue([true])
+    require_numbers = anytrue([true])
+    require_symbols = anytrue([true])
   }
 
   account_recovery_setting {
     recovery_mechanism {
-      name     = "VerifiedEmail"
+      name     = "verified_email"
       priority = 1
     }
-  }
-
-  # default cognito mail to send verification message
-  email_configuration {
-    from_email_address = "no-reply@verificationemail.com"
   }
 }
 
 # configuration of the cognito hosted ui.need to be configured
 
 resource "aws_cognito_user_pool_domain" "commit_cognito_hosted_ui" {
-  domain        = "commit_project"
+  domain        = "commit-project"
   user_pool_id  = aws_cognito_user_pool.commit_users.id
 }
 
